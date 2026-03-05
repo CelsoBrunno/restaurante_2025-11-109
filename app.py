bf17eaeab31a34451ecba03568a226202d93b360
Fernando Abadia Marcelino <fernando58197376@edu.df.senac.br>
2026-03-05 10:55:01 -0300

    gta6

--- app.py
+++ app.py
@@ -20,11 +20,11 @@ def exibir_nome_do_programa():
 ██████╦╝██║░░░██║██████╔╝██║░░██╗░██║░░░██║█████╗░░██████╔╝
 ██╔══██╗██║░░░██║██╔══██╗██║░░╚██╗██║░░░██║██╔══╝░░██╔══██╗
 ██████╦╝╚██████╔╝██║░░██║╚██████╔╝╚██████╔╝███████╗██║░░██║
-╚═════╝░░╚═════╝░╚═╝░░╚═╝░╚═════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝
+╚═════╝░░╚═════╝░╚═╝░░╚═╝░╚═════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝j
 """)
 
 def exibir_opcoes():
-    print('1. Cadastrar loja')
+    print('1. Cadastrar restaurante')
     print('2. Listar restaurantes')
     print('3. Alternar estado do restaurante')
     print('4. Adicionar avaliação')
