b28ae1643dfac74003216d2f29fc955fb32174d5
Celso Brunno Rocha Custódio <110001340+CelsoBrunno@users.noreply.github.com>
2026-06-18 15:09:51 -0300

    Update prato.py

--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -1,10 +1,9 @@
 from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Prato(ItemCardapio):
-    
     def __init__(self, nome, preco, descricao):
         super().__init__(nome, preco)
-        self.descricao = descricao
+        self._descricao = descricao
 
     def __str__(self):
-        return f'{self.nome} | {self.preco} | {self.descricao}'
+        return f'{self._nome} | R$ {self._preco:.2f} | {self._descricao}'
