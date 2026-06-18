043a0126227ec9d221c3e502362861471656e72c
Celso Brunno Rocha Custódio <110001340+CelsoBrunno@users.noreply.github.com>
2026-06-18 15:09:26 -0300

    Update bebida.py

--- modelos/cardapio/bebida.py
+++ modelos/cardapio/bebida.py
@@ -1,10 +1,9 @@
 from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Bebida(ItemCardapio):
-    
     def __init__(self, nome, preco, tamanho):
         super().__init__(nome, preco)
-        self.tamanho = tamanho
+        self._tamanho = tamanho
 
     def __str__(self):
-        return f'{self.nome} | {self.preco} | {self.tamanho}'
+        return f'{self._nome} | R$ {self._preco:.2f} | {self._tamanho}'
